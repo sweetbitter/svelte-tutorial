d034b36bd2fa90aaa071eeb65effb6545f64cfc9
hoseok <kihs1080@gmail.com>
2021-06-06 14:48:06 +0900

    leture 5

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,53 @@
 <script>
-	export let name;
+  import { loop_guard } from 'svelte/internal';
+
+  export let name;
+  let firstName = 'Jimi';
+  let lastName = 'Handrix';
+  let beltColor = 'black';
+  $: fullName = `${firstName} ${lastName}`;
+  $: {
+    console.log(beltColor);
+    console.log(fullName);
+  }
+
+  const handleClick = () => {
+    beltColor = 'orange';
+  };
+  const handleInput = (e) => {
+    beltColor = e.target.value;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <!-- <p style="color:{beltColor}">{beltColor} belt</p> -->
+  <!-- <button on:click={handleClick}>update velt color</button> -->
+  <!-- <input type="text" on:input={handleInput} value={beltColor} /> -->
+  <p>{fullName} - {beltColor} belt</p>
+  <input type="text" bind:value={firstName} />
+  <input type="text" bind:value={lastName} />
+  <input type="text" bind:value={beltColor} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
